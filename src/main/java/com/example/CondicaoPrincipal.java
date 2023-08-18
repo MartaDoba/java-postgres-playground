39d41ee467b696a38393a4e5b6ed264a52e23251
Marta Doba <martadoba@yahoo.com.br>
2023-08-18 02:41:53 +0000

    Melhorando o código

---
+++ src/main/java/com/example/CondicaoPrincipal.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class CondicaoPrincipal {
+    public static void main(String[] args) {
+        UsandoCondicional usandoCondicional = new UsandoCondicional();
+        usandoCondicional.a = 1;
+        usandoCondicional.b = 2;
+        
+        usandoCondicional.calculos();
+    }
+    
+}
---
+++ src/main/java/com/example/UsandoCondicional.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class UsandoCondicional {
+    int a;
+    int b;
+    int c;
+    float media;
+    float multiplicacao;
+    void calculos (){
+        if (a == b){
+            System.out.println("A é igual a b!" + a); 
+            System.out.println("O valor de A é diferente de B" + b);
+            
+        }
+    }
+    
+}
